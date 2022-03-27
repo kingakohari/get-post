697174d7238e9dd92371d7e82cb93791f196b9bc
Kinga Kohari <kinguca@yahoo.com>
2022-03-27 13:36:00 +0200

    Adding CSS to the list

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -13,10 +13,19 @@ function loadEvent() {
       while (data.cards[i]) {
           
           document.getElementById("root").insertAdjacentHTML("beforeend", `
-          <div class="card">
-                  <h2>${data.cards[i].title}</h2>
-                  <p>${data.cards[i].sub}</p>
-                  <p>${data.cards[i].text}</p>
+          <div class="a-box">
+            <div class="img-container">
+              <div class="img-inner">
+                <div class="inner-skew">
+                  <img src="https://images.unsplash.com/photo-1584791097929-aa825fe8e1e1?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ">
+                </div>
+              </div>
+            </div>
+            <div class="text-container">
+                    <h2>${data.cards[i].title}</h2>
+                    <p>${data.cards[i].sub}</p>
+                    <p>${data.cards[i].text}</p>
+            </div>
           </div>
           `);
           i++;
