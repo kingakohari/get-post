98604d4c0c7e9ed428c7de2512a2f46f2842f4dd
Kinga Kohari <kinguca@yahoo.com>
2022-03-28 02:15:07 +0200

    Beer cards styled

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -8,6 +8,21 @@ function loadEvent() {
     .then(function (data) {
       console.log(data)
       
+    let imgArray  = new Array();
+
+    
+    imgArray[0] = "./lala/MangoBay.jpg"
+    imgArray[1] = "./lala/TavoliGalaxis.jpg"
+    imgArray[2] = "./lala/monyo-flying-rabbit.jpg"
+    imgArray[3] = "./lala/LiquidCocaine.jpg"
+    imgArray[4] = "./lala/organic-chocolate.jpg"
+    imgArray[5] = "./lala/Bracia.jpg"
+    imgArray[6] = "./lala/OatmealStout.jpg"
+    imgArray[7] = "./lala/BlackTokyo.jpg"
+    imgArray[8] = "./lala/VintageAle.jpg"
+    imgArray[9] = "./lala/TempestLeaves.jpg"
+
+
       let i = 0
 
       while (data.cards[i]) {
@@ -17,14 +32,14 @@ function loadEvent() {
             <div class="img-container">
               <div class="img-inner">
                 <div class="inner-skew">
-                  <img src="https://images.unsplash.com/photo-1584791097929-aa825fe8e1e1?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ">
+                  <img src=${imgArray[i]}></img>
                 </div>
               </div>
             </div>
             <div class="text-container">
                     <h2>${data.cards[i].title}</h2>
                     <p>${data.cards[i].sub}</p>
-                    <p>${data.cards[i].text}</p>
+                    <p2>${data.cards[i].text}</p2>
             </div>
           </div>
           `);
